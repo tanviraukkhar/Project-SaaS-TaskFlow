1559131d4cc034ad52c64dcf7dc408e0a75491a1
Tanvir Aukkhar <tanviraukkhar009@gmail.com>
2026-08-07 15:51:35 +0600

    Update frontend API URL for production

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-saas-taskflow.onrender.com/api",
 });
 
-
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -14,8 +12,6 @@ API.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
-
 
 export default API;
